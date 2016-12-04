143f3c8c31cae98c709ceccde447a8037fcd7793
GuDaddy <gudaddy@uchicago.edu>
2016-12-04 15:53:36 -0600

    Update chrome extension

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -4,10 +4,10 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     toggle = !toggle;
     if (toggle) {
         console.log("GU ME DADDY");
-        chrome.browswerAction.setIcon({path: "img/on.png", tabID: tab.id});
+        chrome.browswerAction.setIcon({path: "img/on.png"});
     } else {
         console.log("NO MORE GU");
-        chrome.browswerAction.setIcon({path: "img/off.png", tabID: tab.id});
+        chrome.browswerAction.setIcon({path: "img/off.png"});
     }
     chrome.tabs.executeScript(tab.id, {file: "content.js"});
 });
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,14 +1,8 @@
-var toggle;
-
-function replaceImages() {
-    var images = document.getElementsByTagName("img");
-    for (var i = 0; i < images.length; i++) {
-        images[i].src = "https://gu-daddy.github.io/gu-daddy/images/gu01.jpg";
-    }
-}
-
 chrome.runtime.sendMessage({type: "toggle"}, function(response) {
     if (response.toggle) {
-        replaceImages();
+        var images = document.getElementsByTagName("img");
+        for (var i = 0; i < images.length; i++) {
+            images[i].src = "https://gu-daddy.github.io/gu-daddy/images/gu01.jpg";
+        }
     }
 });
